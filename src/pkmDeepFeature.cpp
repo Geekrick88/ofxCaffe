5bd37ef85cdfaefa4d357a1fb17481c11a6748a6
pkm <parag@pkmital.com>
2015-01-12 00:20:09 -0500

    updating for latest of-master

---
+++ src/pkmDeepFeature.cpp
@@ -0,0 +1,9 @@
+//
+//  pkmRCNN.cpp
+//  ofxCaffe-RCNN
+//
+//  Created by Mr. Magoo on 1/9/15.
+//
+//
+
+#include "pkmRCNN.h"
---
+++ src/pkmDeepFeature.h
@@ -0,0 +1,14 @@
+//
+//  pkmRCNN.h
+//  ofxCaffe-RCNN
+//
+//  Created by Mr. Magoo on 1/9/15.
+//
+//
+
+#ifndef __ofxCaffe_RCNN__pkmRCNN__
+#define __ofxCaffe_RCNN__pkmRCNN__
+
+#include <stdio.h>
+
+#endif /* defined(__ofxCaffe_RCNN__pkmRCNN__) */
